d93a15972b2081bfa305e090c40ce31dc7ef9e56
Jay Prakash <unijay12@gmail.com>
2021-02-08 00:18:58 +0530

    Course section created

---
+++ src/View/Course.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <Navbar />
+    <div class="container-fluid bg-white text-dark">
+      <div class="row d-flex">
+        <div
+          class="order-1 order-md-1 col-12 col-md-2 col-lg-2 py-3 bg-sidebar-light border-right"
+        >
+          <SecondNav activeTab='Course' />
+        </div>
+        <div
+          class="order-2 order-md-2 col-12 col-md-10 col-lg-10 min-height min-page-height py-3"
+        >
+          <div class="">
+            <h1 class="d-block">Courses</h1>
+            <div class="row">
+          <courseCard />
+          </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Navbar from "@/components/Navbar.vue";
+import SecondNav from "@/components/SecondNav.vue";
+import courseCard from "@/components/CourseCard.vue"
+export default {
+  name: "course",
+  components: {
+    Navbar,
+    SecondNav,
+    courseCard,
+  },
+};
+</script>
+<style scoped>
+.d-block {
+  display: block;
+}
+.bg-sidebar-light {
+    background-color: #e9ecef!important;
+}
+.border-right {
+    border-right: 1px solid #dee2e6!important;
+}
+.pb-3, .py-3 {
+    padding-bottom: 1rem!important;
+}
+.pt-3, .py-3 {
+    padding-top: 1rem!important;
+}
+</style>
