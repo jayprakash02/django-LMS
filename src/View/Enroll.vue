2d61d9d9d8456fe9ed2c4ed4011053f23e35a81f
Jay Prakash <unijay12@gmail.com>
2021-02-08 00:18:45 +0530

    lank enroll page created

---
+++ src/View/Enroll.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Navbar />
+    <h1>Enroll</h1>
+  </div>
+</template>
+
+<script>
+import Navbar from '@/components/Navbar.vue'
+export default {
+    name: "enroll",
+  components: {
+    Navbar,
+    },
+}
+</script>
+<style scoped>
+
+</style>
