67576bab76d7942ec234ae8472e1b23b2c7b3d8f
Jay Prakash <unijay12@gmail.com>
2021-02-08 00:19:17 +0530

    Update router.js

--- src/router.js
+++ src/router.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/View/Home.vue'
+import Course from '@/View/Course.vue'
+import Project from '@/View/Project.vue'
+import Enroll from '@/View/Enroll.vue'
+import Login from '@/View/Login.vue'
+import Blog from '@/View/Blog.vue'
+
 Vue.use(Router)
 
 export default new Router({
@@ -9,6 +15,31 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/course',
+      name: 'course',
+      component: Course
+    },
+    {
+      path: '/project',
+      name: 'project',
+      component: Project
+    },
+    {
+      path: '/enroll',
+      name: 'enroll',
+      component: Enroll
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/blog',
+      name: 'blog',
+      component: Blog
     },
   ]
 })
