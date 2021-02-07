e1a247035a17163ab5673bd30bd0b81d6dd08b40
Jay Prakash <unijay12@gmail.com>
2021-02-08 00:17:21 +0530

    Cards for project created

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="col-12 col-md-6 col-lg-4 mb-3">
+    <div class="card border-0 false">
+      <a href="#"
+        ><img
+          src="@/assets/projectcard.jpg"
+          class="card-img-top"
+          alt="FastAPI &amp; Airtable logo"
+      /></a>
+      <div class="card-body false">
+        <h4 class="card-title">
+          <a href="#">FastAPI &amp; Airtable</a>
+        </h4>
+        <div class="undefined markdown-preview jm-markdown">
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error beatae officiis tempora optio quibusdam vero a neque laboriosam at consequuntur!
+          </p>
+         <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error beatae officiis tempora optio quibusdam vero a neque laboriosam at consequuntur!
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error beatae officiis tempora optio quibusdam vero a neque laboriosam at consequuntur!
+          </p>
+          <ul>
+            <li>Python 3.6 (and up)</li>
+            <li>FastAPI</li>
+            <li>Python Requests</li>
+            <li>FastAPI Forms</li>
+            <li>Jinja Templates</li>
+            <li>And more</li>
+          </ul>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error beatae officiis tempora optio quibusdam vero a neque laboriosam at consequuntur!
+          </p>
+          <p>
+            <a
+              target="_self"
+              href="#"
+              >Code Reference</a
+            >
+          </p>
+        </div>
+        <a class="btn btn-cfe mt-2" href="#">View</a>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "projectCard",
+};
+</script>
+<style scoped>
+.markdown-preview {
+    min-height: 0!important;
+    max-height: 150px!important;
+    overflow-y: scroll;
+}
+.btn-cfe {
+    background-color: #007cae;
+    color: #fff;
+}
+</style>
