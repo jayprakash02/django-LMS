a7401b610b4833b06516491989419217fc500281
Jay Prakash <unijay12@gmail.com>
2021-02-08 00:17:33 +0530

    Navbar created

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,48 +1,35 @@
 <template>
-  <div>
-    <nav class="bg-cfe navbar-dark d-flex navbar navbar-expand-md">
-      <a aria-current="page" class="navbar-brand mr-2 active" href="/"
-        ><img
+    <div>
+      <b-navbar toggleable="lg" type="dark" variant="info" class="bg">
+        <b-navbar-brand to='/'><img
           src="@/assets/logo.svg"
           class="d-inline-block align-top mr-1"
           width="30"
           height="30"
           alt="Coding for Entrepreneurs nav logo"
-      /></a>
-      <form class="my-0 form d-none d-md-inline w-25">
-        <input
-          class="border-0 form-control mr-md-2 bg-black text-light"
-          type="text"
-          name="q"
-          placeholder="Search"
-          aria-label="Search"
-          value=""
-        />
-      </form>
-      <button type="button" class="navbar-toggler">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" style="">
-        <ul class="ml-auto text-center navbar-nav">
-          <li class="nav-item">
-            <a aria-current="page" class="nav-link active" href="/">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/courses">Courses</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/projects">Projects</a>
-          </li>
-          <li class="nav-item"><a class="nav-link" href="/login">Login</a></li>
-          <li class="nav-item">
-            <a class="nav-link bg-warning text-dark rounded" href="/enroll"
-              >Enroll</a
-            >
-          </li>
-        </ul>
-      </div>
-    </nav>
-  </div>
+      /></b-navbar-brand>
+
+        <b-navbar-toggle class="text" target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+          <b-nav-form class="center">
+            <b-form-input
+              size="sm"
+              class="mr-sm-2"
+              placeholder="Search"
+            ></b-form-input>
+          </b-nav-form>
+
+          <!-- Right aligned nav items -->
+          <b-navbar-nav class="ml-auto" >
+            <b-nav-item to='/course' class="center" Right>Course</b-nav-item>
+            <b-nav-item to='/project' class="center" Right>Project</b-nav-item>
+            <b-nav-item to='/login' class="center" Right>Login</b-nav-item>
+            <b-nav-item to='/enroll' Right class="bg-warning rounded center">Enroll</b-nav-item>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
+    </div>
 </template>
 
 <script>
@@ -53,5 +40,12 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+.bg {
+    background-color: #007cae!important;
+    color: #fff!important;
+}
+.center{
+  text-align: center;
+  justify-content: center;
+}
 </style>
