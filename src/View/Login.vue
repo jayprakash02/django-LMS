a76022eb3e3f289ee0444cda54f3e1ac6ebfafda
Jay Prakash <unijay12@gmail.com>
2021-02-08 00:18:16 +0530

    Login section Created

---
+++ src/View/Login.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Navbar />
+    <h1>Login</h1>
+  </div>
+</template>
+
+<script>
+import Navbar from '@/components/Navbar.vue'
+export default {
+    name: "login",
+  components: {
+    Navbar,
+    },
+}
+</script>
+<style scoped>
+
+</style>
