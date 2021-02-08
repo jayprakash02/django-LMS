0d1a0d72fbef7b4dc66fe9b9258fde5fa7402eb1
Jay Prakash <unijay12@gmail.com>
2021-02-08 23:12:12 +0530

    Update Course.vue

--- src/View/Course.vue
+++ src/View/Course.vue
@@ -6,16 +6,18 @@
         <div
           class="order-1 order-md-1 col-12 col-md-2 col-lg-2 py-3 bg-sidebar-light border-right"
         >
-          <SecondNav activeTab='Course' />
+          <SecondNav activeTab="Course" />
         </div>
         <div
           class="order-2 order-md-2 col-12 col-md-10 col-lg-10 min-height min-page-height py-3"
         >
           <div class="">
-            <h1 class="d-block">Courses</h1>
+            <div class="d-flex">
+              <div class="flex-grow-1"><h1>Course</h1></div>
+            </div>
             <div class="row">
-          <courseCard />
-          </div>
+              <courseCard />
+            </div>
           </div>
         </div>
       </div>
@@ -26,7 +28,7 @@
 <script>
 import Navbar from "@/components/Navbar.vue";
 import SecondNav from "@/components/SecondNav.vue";
-import courseCard from "@/components/CourseCard.vue"
+import courseCard from "@/components/CourseCard.vue";
 export default {
   name: "course",
   components: {
@@ -41,15 +43,17 @@ export default {
   display: block;
 }
 .bg-sidebar-light {
-    background-color: #e9ecef!important;
+  background-color: #e9ecef !important;
 }
 .border-right {
-    border-right: 1px solid #dee2e6!important;
+  border-right: 1px solid #dee2e6 !important;
 }
-.pb-3, .py-3 {
-    padding-bottom: 1rem!important;
+.pb-3,
+.py-3 {
+  padding-bottom: 1rem !important;
 }
-.pt-3, .py-3 {
-    padding-top: 1rem!important;
+.pt-3,
+.py-3 {
+  padding-top: 1rem !important;
 }
 </style>
