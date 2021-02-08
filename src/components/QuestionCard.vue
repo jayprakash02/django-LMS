068fc609d5ce8cafb3ac962f7702948fed9600b2
Jay Prakash <unijay12@gmail.com>
2021-02-08 23:11:28 +0530

    Create QuestionCard.vue

---
+++ src/components/QuestionCard.vue
@@ -0,0 +1,46 @@
+<template>
+  <a
+    class="list-group-item false"
+    href="#"
+    ><div class="row">
+      <div class="col-2 col-md-1 text-center">
+        <div class="text-avatar-img-x text-avatar"><span>A</span></div>
+      </div>
+      <div class="col">
+        <span width="500"
+          ><span
+            ><span
+              ><span
+                >following an order sometimes let's encrypts do not leave the
+                data pa<span>...</span></span
+              ></span
+            ></span
+          ><span
+            style="position: fixed; visibility: hidden; top: 0px; left: 0px"
+            ><span>...</span></span
+          ></span
+        >
+        <p class="py-0 my-0">
+          <small class="text-muted"><span>4 days ago </span></small>
+        </p>
+      </div>
+    </div></a
+  >
+</template>
+<script>
+export default {
+  name: "questionCard",
+};
+</script>
+<style scoped>
+.text-avatar {
+    background: #ccc;
+    border-radius: 1.5em;
+    color: #fff;
+    display: inline-block;
+    font-weight: 700;
+    line-height: 3em;
+    text-align: center;
+    width: 3em;
+}
+</style>
