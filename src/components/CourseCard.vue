f73565cabe9645553dd4c90a891518f7ee52ca01
Jay Prakash <unijay12@gmail.com>
2021-02-10 00:25:09 +0530

    props added for itteration

--- src/components/CourseCard.vue
+++ src/components/CourseCard.vue
@@ -3,27 +3,29 @@
     <div
       class="card mb-5 pt-3 pb-5 rounded-0 border-top-0 border-right-0 border-left-0"
     >
-      <div class="col-md-4">
-        <a class="mr-2" href="/courses/ecommerce"
-          ><img
-            src="@/assets/card.jpg"
-            class="w-100"
-            alt="course eCommerce thumbnail"
-        /></a>
-      </div>
-      <div class="col-md-8 px-3">
-        <div class="card-block px-3">
-          <h4 class="card-title undefined">
-            <a class="mr-2" href="/courses/ecommerce">eCommerce</a>
-          </h4>
-          <div class="markdown-preview jm-markdown">
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
-              non error impedit totam natus ex quo tempora blanditiis,
-              cupiditate mollitia.
-            </p>
+      <div class="row">
+        <div class="col-md-4">
+          <a class="mr-2" href="#"
+            ><img
+              :src="imageUrl"
+              class="w-100"
+              :alt="title"
+          /></a>
+        </div>
+        <div class="col-md-8 px-3">
+          <div class="card-block px-3">
+            <h4 class="card-title undefined">
+              <a class="mr-2" href="#">{{ title }}</a>
+            </h4>
+            <div class="markdown-preview jm-markdown">
+              <p>
+                {{ description }}
+              </p>
+            </div>
+            <a class="mr-2 btn btn-outline-cfe" href="#"
+              >View</a
+            >
           </div>
-          <a class="mr-2 btn btn-outline-cfe" href="/courses/ecommerce">View</a>
         </div>
       </div>
     </div>
@@ -32,5 +34,18 @@
 <script>
 export default {
   name: "courseCard",
+  props:{
+    title:String,
+    description:String,
+    imageUrl:String
+  }
 };
 </script>
+<style scoped>
+.btn-outline-cfe {
+    color: #007cae!important;
+    background-color: initial;
+    background-image: none;
+    border-color: #007cae!important;
+}
+</style>
