8868e04e97f82219bd167ab9cdbeeaee4afaa7ac
Jay Prakash <unijay12@gmail.com>
2021-02-10 00:27:12 +0530

    serializers created to serve course model

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,1 +1,7 @@
 from rest_framework import serializers
+from .models import *
+
+class CourseSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Course
+        fields = ('url', 'title', 'desc', 'image', 'timestamp')
