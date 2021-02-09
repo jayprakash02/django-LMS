3400ee76e13ed5acdaaf68698f0ec425faf49e8b
Jay Prakash <unijay12@gmail.com>
2021-02-10 00:25:29 +0530

    course store registered

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import courses from '@/store/modules/courseStore.js'
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
-
+    courses,
   }
 })
