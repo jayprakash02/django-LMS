ee85c36b72e8a1c13e420bf492b621053c3f0eeb
Jay Prakash <unijay12@gmail.com>
2021-02-10 00:27:40 +0530

    small changes

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Course,Video,Comment,Author
+from .models import Course,Video,Comment,Author,Doc
 # Register your models here.
 admin.site.register(Course)
 admin.site.register(Video)
 admin.site.register(Comment)
 admin.site.register(Author)
+admin.site.register(Doc)
