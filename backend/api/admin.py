c1d2a576cda4ab7581f2060afe8f934a01c4a62e
Jay Prakash <unijay12@gmail.com>
2021-02-10 22:52:10 +0530

    added writing tool to all desc model

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
-admin.site.register(Course)
 admin.site.register(Video)
 admin.site.register(Comment)
 admin.site.register(Author)
@@ -9,4 +8,14 @@ admin.site.register(Author)
 @admin.register(Doc)
 class DocAdmin(admin.ModelAdmin):
     class Media:
-        js= ('tinyInject.js',)
+        js= ('admin/doc.js',)
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    class Media:
+        js= ('admin/doc.js',)
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    class Media:
+        js= ('admin/doc.js',)
