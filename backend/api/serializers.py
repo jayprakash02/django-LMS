8d3ddc012c1167dbc7fa3f459b9c1901051b9913
Jay Prakash <unijay12@gmail.com>
2021-02-10 19:26:10 +0530

    serializer and viewset creted for projects

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,7 +1,22 @@
 from rest_framework import serializers
 from .models import *
 
+class VideoSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Video
+        fields = ('url', 'title', 'desc', 'image', 'timestamp','video')
+
+class DocSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Doc
+        fields = ('url', 'title', 'desc', 'timestamp')
+
 class CourseSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Course
-        fields = ('url', 'title', 'desc', 'image', 'timestamp')
+        fields = ('url', 'title', 'desc', 'image', 'timestamp','video','doc')
+
+class ProjectSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Project
+        fields = ('url', 'title', 'desc', 'image', 'timestamp','video','doc')
--- backend/api/viewsets.py
+++ backend/api/viewsets.py
@@ -7,6 +7,18 @@ from .models import *
 # Serve Vue Application
 index_view = never_cache(TemplateView.as_view(template_name='index.html'))
 
+class VideoViewSet(viewsets.ModelViewSet):
+    queryset = Video.objects.all()
+    serializer_class = VideoSerializer
+
+class DocViewSet(viewsets.ModelViewSet):
+    queryset = Doc.objects.all()
+    serializer_class = DocSerializer
+
 class CourseViewSet(viewsets.ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
