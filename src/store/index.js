c7de17f2b8e817600af5c2d260467c87f81faa1c
Jay Prakash <unijay12@gmail.com>
2021-02-11 23:35:19 +0530

    login view ready

--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,7 @@ import Vuex from 'vuex'
 import courses from '@/store/modules/courseStore.js'
 import projects from '@/store/modules/projectStore.js'
 import videos from '@/store/modules/videoStore.js'
-import auth from '@store/modules/auth.js'
+import auth from '@/store/modules/auth.js'
 
 Vue.use(Vuex)
 
